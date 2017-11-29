4ae6dbb7f940b0bd111a38cafdeace9bb89c0887
Olaf Lessenich <lessenic@fim.uni-passau.de>
2017-11-29 18:50:06 +0100

    Run a job for each file triple by setting RUN=true

--- git-jdime.sh
+++ git-jdime.sh
@@ -6,22 +6,27 @@
 set -eu
 
 if [ "$#" -lt 2 ]; then
-    IFS=" " read left right <<< $(git log --pretty=%P -n1 $1)
+	IFS=" " read left right <<< $(git log --pretty=%P -n1 $1)
 else
-    left="$1"
-    right="$2"
+	left="$1"
+	right="$2"
 fi
 
 trap "echo Something went wrong; exit $?" INT TERM EXIT
 target=$(git preparemerge $left $right /tmp/tmp.jdime)
 trap - INT TERM EXIT
 
-for strategy in linebased structured; do
-    out=${target}/${strategy}
-    jdimeargs="-eoe -m $strategy -r -q -o ${out}"
-    trap "echo $target; exit $?" INT TERM EXIT
-    jdime ${jdimeargs} ${target}/{left,base,right}
-    trap - INT TERM EXIT
-done
+errlog=${target}/$$.err
+out=${target}/out
+
+while read cmd; do
+	echo $cmd
+	if not $cmd > $out 2>&1; then
+		date >> $errlog
+		echo $cmd >> $errlog
+		cat $out | tee -a $errlog
+		echo >> $errlog
+	fi
+done < ${target}/run.sh
 
 echo "$target"
--- git-preparemerge.sh
+++ git-preparemerge.sh
@@ -10,11 +10,16 @@ right="$2"
 
 target=${3:-""}
 if [ -z "$target" ]; then
-    target=$(mktemp -d)
+	target=$(mktemp -d)
 elif [ -d $target ]; then
-    echo "$target already exists! Exiting."; exit 1
+	echo "$target already exists! Exiting."; exit 1
 fi
 mkdir -p ${target}/{left,base,right}
+
+strategies="linebased structured"
+for strategy in $strategies; do
+	mkdir -p ${target}/${strategy}
+done
 
 base=$(git merge-base $left $right)
 
@@ -22,13 +27,36 @@ merged=$(git diff --name-status --diff-filter=M $left $right | awk '{ print $2 }
 #added=$(git diff --name-status --diff-filter=A $left $right | awk '{ print $2 }')
 #deleted=$(git diff --name-status --diff-filter=D $left $right | awk '{ print $2 }')
 
+run=${RUN:-false}
+jdimeargs="-eoe -q"
+cmdfile=${target}/run.sh
+
 for f in $merged; do
-    path=$(dirname $f)
-    filename=$(basename $f)
-    mkdir -p ${target}/{left,base,right}/${path}
-    git show ${base}:$f > ${target}/base/${path}/${filename} || touch ${target}/base/${path}/${filename}
-    git show ${left}:$f > ${target}/left/${path}/${filename}
-    git show ${right}:$f > ${target}/right/${path}/${filename}
+	path=$(dirname $f)
+	filename=$(basename $f)
+	mkdir -p ${target}/{left,base,right}/${path}
+	lfile=${target}/left/${path}/${filename}
+	bfile=${target}/base/${path}/${filename}
+	rfile=${target}/right/${path}/${filename}
+	git show ${base}:$f > $bfile || touch $bfile
+	git show ${left}:$f > $lfile
+	git show ${right}:$f > $rfile
+
+	if $run; then
+		for strategy in $strategies; do
+			outfile=${target}/${strategy}/${path}/${filename}
+			mkdir -p $(dirname $outfile)
+			echo "jdime -m ${strategy} ${jdimeargs} -o ${outfile} ${lfile} ${bfile} ${rfile}" >> ${cmdfile}
+		done
+	fi
+
 done
+
+if not $run; then
+	for strategy in $strategies; do
+		out=${target}/${strategy}
+		echo jdime -m ${strategy} ${jdimeargs} -r -o ${target}/${strategy} ${target}/{left,base,right} >> ${cmdfile}
+	done
+fi
 
 echo "${target}"
