8f2acad0801ff73ae0c96c000cfe02bb4d34f413
Olaf Lessenich <lessenic@fim.uni-passau.de>
2017-11-30 00:38:56 +0100

    Reimplement git-preparemerge in python

--- git-jdime.sh
+++ git-jdime.sh
@@ -5,28 +5,7 @@
 
 set -eu
 
-if [ "$#" -lt 2 ]; then
-	IFS=" " read left right <<< $(git log --pretty=%P -n1 $1)
-else
-	left="$1"
-	right="$2"
-fi
-
-trap "echo Something went wrong; exit $?" INT TERM EXIT
-target=$(git preparemerge $left $right /tmp/tmp.jdime)
-trap - INT TERM EXIT
-
-errlog=${target}/$$.err
-out=${target}/out
-
 while read cmd; do
 	echo $cmd
-	if not $cmd > $out 2>&1; then
-		date >> $errlog
-		echo $cmd >> $errlog
-		cat $out | tee -a $errlog
-		echo >> $errlog
-	fi
-done < ${target}/run.sh
-
-echo "$target"
+	$cmd || echo "Failed with $?."
+done < <(git preparemerge -o /tmp/tmp.jdime $@)
---
+++ git_preparemerge.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2017 Olaf Lessenich
+
+import argparse
+import collections
+import os
+import sys
+import tempfile
+from plumbum import local
+
+
+#STRATEGIES = ['linebased', 'structured']
+STRATEGIES = ['structured']
+GIT = local['git']
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def get_merged_files(revs):
+    merged_files = GIT['diff', '--name-status', '--diff-filter=M',
+                       revs['left'], revs['right']]().splitlines()
+    return map(lambda x: x[2:], merged_files)
+
+def prepare_job(target, file, revs):
+    path = os.path.dirname(file)
+    
+    for rev in STRATEGIES:
+        os.makedirs(os.path.join(target, rev, path), exist_ok=True)
+    
+    for rev, commit in revs.items():
+        os.makedirs(os.path.join(target, rev, path), exist_ok=True)
+        with open(os.path.join(target, rev, file), 'w') as targetfile:
+            targetfile.write(GIT['show', commit + ":" + file]())
+
+def write_job(target, file, revs):
+    inputfiles = []
+    for rev in revs.keys():
+        inputfiles.append(os.path.join(target, rev, file))
+    for strategy in STRATEGIES:
+        outfile = os.path.join(target, strategy, file)
+        print('jdime -eoe -m %s -o %s %s' % (strategy,
+                                             outfile,
+                                             ' '.join(inputfiles)))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output',
+                        help='Store output in this directory',
+                        type=str)
+    parser.add_argument('commits', default=[], nargs='+')
+    args = parser.parse_args()
+
+    revs = collections.OrderedDict()
+
+    commits = args.commits
+
+    if len(commits) is 1:
+        # Only mergecommit is specified. We need to compute left and right.
+        left, right = GIT['log', '--pretty=%P', '-n1',
+                          commits[0]]().strip().split(' ')
+    else:
+        # Left and right are provided.
+        left = commits[0]
+        right = commits[1]
+    
+    revs['left'] = left
+    # TODO: handle two-way merges
+    revs['base'] = GIT['merge-base', left, right]().strip()
+    revs['right'] = right
+
+    if args.output:
+        target = args.output
+
+        if os.path.exists(target):
+            eprint('Error! Directory exists: %s\nExiting.' % target)
+            sys.exit(1)
+    else:
+        target = tempfile.mkdtemp(prefix="jdime.")
+
+    for file in get_merged_files(revs):
+        prepare_job(target, file, revs)
+        write_job(target, file, revs)
+
+if __name__ == "__main__":
+    main()
