e9d84bbb34ed617c86fb7b6e9244c8244903f361
Olaf Lessenich <lessenic@fim.uni-passau.de>
2017-12-01 13:20:38 +0100

    Improve handling of errors in git-jdime

--- git-jdime.sh
+++ git-jdime.sh
@@ -3,9 +3,25 @@
 # Copyright (C) 2017 Olaf Lessenich
 #
 
-set -eu
+set -u
+
+TMPLOG=$(mktemp)
+: "${OUTPUT=/tmp/tmp.jdime.$(date +%Y%m%d%H%M%S)}"
+: "${LOG:=${OUTPUT}/error.log}"
 
 while read cmd; do
 	echo $cmd
-	$cmd || echo "Failed with $?."
-done < <(git preparemerge -o /tmp/tmp.jdime $@)
+	$cmd > $TMPLOG 2>&1
+	ret=$?
+	if [ "$ret" != "0" ]; then
+		(printf '=%.0s' {1..80}; echo "") >> $LOG
+		date >> $LOG
+		echo "$cmd" >> $LOG
+		(printf -- '-%.0s' {1..80}; echo "") >> $LOG
+		cat $TMPLOG >> $LOG
+		(printf -- '-%.0s' {1..80}; echo "") >> $LOG
+		echo "Failed with ${ret}." | tee -a $LOG
+		echo >> $LOG
+	fi
+	rm "$TMPLOG"
+done < <(git preparemerge -o $OUTPUT $@)
