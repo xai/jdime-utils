0f4cea84e9578f03f01cf73a799909fb07f9a087
Olaf Lessenich <lessenic@fim.uni-passau.de>
2017-12-06 17:12:07 +0100

    Provide option to specify file that should be merged

--- git_jdime.py
+++ git_jdime.py
@@ -21,7 +21,7 @@ def get_jobs(target, commits):
     return csv.DictReader(iter(GIT['preparemerge', '-o', target, commits]()\
                                .splitlines()), delimiter=';', fieldnames=COLS)
 
-def run(job, prune):
+def run(job, prune, srcfile=None):
     project = job['project']
     left = job['left'][0:7]
     right = job['right'][0:7]
@@ -29,29 +29,30 @@ def run(job, prune):
     target = job['target']
 
     fail = False
-    errorlog = os.path.join(target, 'error.log')
 
-    strategies = job['strategies'].split(',')
-    for strategy in strategies:
-        scenario = '%s %s %s %s %s' % (project, left, right, file, strategy)
-        print('%s: ' % scenario, end='')
-        cmd = job['cmd'].replace(STRATEGY, strategy).split(' ')
-        exe = cmd[0]
-        args = cmd[1:]
+    if not srcfile or srcfile == file:
+        errorlog = os.path.join(target, 'error.log')
+        strategies = job['strategies'].split(',')
+        for strategy in strategies:
+            scenario = '%s %s %s %s %s' % (project, left, right, file, strategy)
+            print('%s: ' % scenario, end='')
+            cmd = job['cmd'].replace(STRATEGY, strategy).split(' ')
+            exe = cmd[0]
+            args = cmd[1:]
 
-        ret, stdout, stderr = local[exe][args].run(retcode=None)
-        if ret == 0:
-            print(colors.green | 'OK')
-        else:
-            fail = True
-            print(colors.red | ('FAILED (%d)' % ret))
-            with open(errorlog, 'a') as err:
-                err.write(80 * '=' + '\r\n')
-                err.write(scenario + '\r\n')
-                err.write('> %s\r\n' % ' '.join(cmd))
-                err.write(80 * '-' + '\r\n')
-                err.writelines(stderr)
-                err.write(80 * '-' + '\r\n')
+            ret, stdout, stderr = local[exe][args].run(retcode=None)
+            if ret == 0:
+                print(colors.green | 'OK')
+            else:
+                fail = True
+                print(colors.red | ('FAILED (%d)' % ret))
+                with open(errorlog, 'a') as err:
+                    err.write(80 * '=' + '\r\n')
+                    err.write(scenario + '\r\n')
+                    err.write('> %s\r\n' % ' '.join(cmd))
+                    err.write(80 * '-' + '\r\n')
+                    err.writelines(stderr)
+                    err.write(80 * '-' + '\r\n')
 
     if prune and not fail:
         for root, dirs, files in os.walk(target, topdown=False):
@@ -66,6 +67,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--output',
                         help='Store output in this directory',
+                        type=str)
+    parser.add_argument('-f', '--file',
+                        help='Merge only specified file',
                         type=str)
     parser.add_argument('-p', '--prune',
                         help='Prune successfully merged scenarios',
@@ -83,10 +87,10 @@ def main():
     if len(commits) == 1 and commits[0] == 'all':
         for commit in get_merge_commits():
             for job in get_jobs(target, [commit,]):
-                run(job, args.prune)
+                run(job, args.prune, args.file)
     else:
         for job in get_jobs(target, commits):
-            run(job, args.prune)
+            run(job, args.prune, args.file)
 
     if args.prune and not os.listdir(target):
         os.rmdir(target)
